143e02b3b2d5fe3d62d6a1b5f622b7b1ddcf3fa3
mzewdu <menasse.zewdu@nielsen.com>
2020-08-18 17:17:14 -0700

    0.7

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 set -e 
 
-args="--regex --entropy=False --max_depth=15" 
+args="--regex --rules regexes.json --entropy=False" 
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
@@ -10,8 +10,8 @@ fi
 if [ -n "${INPUT_GITHUBTOKEN}" ]; then
   githubRepo="https://$INPUT_GITHUBTOKEN@github.com/$GITHUB_REPOSITORY" # Overwrite for private repository if token provided
 else
-  #githubRepo="https://github.com/$GITHUB_REPOSITORY" # Default target repository
-  githubRepo="https://github.com/menzew/ClassLoadGrapher" # Default target repository
+  githubRepo="https://github.com/$GITHUB_REPOSITORY" # Default target repository
+  #githubRepo="https://github.com/menzew/ClassLoadGrapher" # Default target repository
 fi
 
 query="$args $githubRepo" # Build args query with repository url
